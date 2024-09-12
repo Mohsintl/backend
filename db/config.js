7ae38b04e02e8ac061c9c8832418f1e7cb25837d
Mohsintl <tlmdmohsin782@gmail.com>
2024-09-13 00:53:33 +0530

    register - onclick registering user data in db

---
+++ db/config.js
@@ -0,0 +1,3 @@
+const mongoose = require( "mongoose");
+const uri = 'mongodb+srv://mohsin782:iPhone782@cluster0.b0jvw.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(uri);
